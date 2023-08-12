a2423aa74b8ad40cb0a0c256250f75288cefd646
DUY HUYNH <124075895+duyhuynhnguyentuan@users.noreply.github.com>
2023-08-12 23:54:10 +0700

    complete navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
 import styles from "./navbar.module.css"
 const Navbar = () => {
   return (
+    <div className={styles.navbar}>
     <nav className={styles.nav}>
-        <div className={styles.logo}></div>
+        <div className={styles.logo}>
+            <img src="./images/DUY HUYNH.png" alt="logo" className={styles.logoImg}/>
+        </div>
         <ul className={styles.navMenu}>
-            <li className={styles.navItem}>Home</li>
-            <li className={styles.navItem}>Product</li>
-            <li className={styles.navItem}>About</li>
+            <li className={styles.navItem}><a href="#">Home</a></li>
+            <li className={styles.navItem}><a href="#">Product</a></li>
+            <li className={styles.navItem}><a href="#">About</a></li>
         </ul>
     </nav>
+    </div>
     
   )
 }
