e258beb56773c4c3b7463b1aadc54f190569b4b7
DUY HUYNH <124075895+duyhuynhnguyentuan@users.noreply.github.com>
2023-08-16 23:52:05 +0700

    add animation to content and navbar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,46 @@
 import styles from "./content.module.css";
-
+import {motion} from 'framer-motion'
 const Content = () => {
+  const animationContent = {
+    hidden:{
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      transition : {
+        staggerChildren: 0.6
+    }
+  }
+};
+const animationTitle ={
+  hidden:{
+    opacity: 0,
+    y: -100,
+  },
+  visible:{
+    opacity: 1,
+    y:0 ,
+    transition:{
+      type: "spring",
+      stiffness: 100,
+    }
+
+  }
+};
+const animationGuitar= {
+  hidden:{
+    opacity: 0,
+    x:200
+  },
+  visible: {
+    opacity: 1,
+    x:0,
+    transition:{
+      type: "spring",
+      stiffness: 100,
+    }
+  }
+}
   return (
     <div className={styles.container}>
         <div className={styles.imgLogo}>
@@ -10,18 +50,26 @@ const Content = () => {
             className={styles.fenderLogo}
             />
         </div>
-      <div className={styles.mainContent}>
-        <div>
+      <motion.div className={styles.mainContent} 
+      variants={animationContent}
+      initial="hidden"
+      animate="visible"
+      >
+        <motion.div variants={animationTitle}>
             <h1>Fender <br/>
             Jazzmaster
             </h1>
-        </div>
-        <div className={styles.fenderguitarContainer}>
+        </motion.div>
+        <motion.div className={styles.
+        fenderguitarContainer
+        }
+        variants={animationGuitar}
+        >
             <img src="https://i.pinimg.com/originals/1d/1e/58/1d1e581df54d1defee522c3cb2bc7aee.png" alt="fenderguitar"
             className={styles.fenderguitar}
              />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,85 @@
 import styles from "./navbar.module.css"
+import {motion} from "framer-motion"
 const Navbar = () => {
+  const animationNavbar = {
+    hidden: {
+      opacity: 0,
+    }, 
+    visible:{
+      opacity:1,
+      transition:{
+        delayChildren: 2,
+        staggerChildren: 0.4
+      }
+    }
+  };
+  const animationNavItem = {
+    hidden:{
+      opacity: 0,
+      y: -20,
+    },
+    visible:{
+      opacity:1,
+      y:0,
+      transition:{
+        ease: "easeIn",
+        duration: 0.2,
+      }
+    }
+  };
+  const animationLogo = {
+    hidden:{
+      scale: 1.4,
+      opacity:0
+    },
+    visible:{
+      scale:1,
+      opacity:1,
+      rotate: "360deg",
+      transition:{
+        delay:1.2,
+        duration:1
+      }
+    }
+  };
+  const containerNav = {
+    hidden:{opacity:0},
+    visible:{opacity:1,
+    transition:{
+      delay: 1.2,
+    }
+    }
+  }
+
   return (
     <div className={styles.navbar}>
-    <nav className={styles.nav}>
+    <motion.nav className={styles.nav}
+    variants={containerNav}
+    initial="hidden"
+    animate="visible"
+    >
         <div className={styles.logo}>
-            <img src="./images/DUY HUYNH.png" alt="logo" className={styles.logoImg}/>
+            <motion.img src="./images/DUY HUYNH.png" alt="logo" className={styles.logoImg}
+            variants={animationLogo}
+            initial="hidden"
+            animate="visible"
+            />
         </div>
-        <ul className={styles.navMenu}>
-            <li className={styles.navItem}><a href="#">Home</a></li>
-            <li className={styles.navItem}><a href="#">Product</a></li>
-            <li className={styles.navItem}><a href="#">About</a></li>
-        </ul>
-    </nav>
+        <motion.ul className={styles.navMenu}
+        variants={animationNavbar}
+        initial="hidden"
+        animate="visible"
+        >
+            <motion.li className={styles.navItem}
+            variants={animationNavItem}
+            ><a href="#">Home</a></motion.li>
+            <motion.li className={styles.navItem}
+               variants={animationNavItem}><a href="#">Product</a></motion.li>
+            <motion.li className={styles.navItem}
+               variants={animationNavItem}
+            ><a href="#">About</a></motion.li>
+        </motion.ul>
+    </motion.nav>
     </div>
     
   )
